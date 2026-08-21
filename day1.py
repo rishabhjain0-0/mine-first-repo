78d86a497d576ee06fe33ae67605e41213ef9303
Rishabh Jain <rishabhjainqt@gmail.com>
2026-08-21 09:59:06 +0530

    day1-theory

---
+++ day1.py
@@ -0,0 +1,55 @@
+'''      #1
+salary=50.5
+print(salary)
+print(type(salary)
+'''
+#2 
+'''
+a=2e2
+b=2E2
+c=2e3
+print(a)
+print(b)
+print(c)
+print(type(a))
+'''
+#3
+'''a=3+5j
+b=2-5.5j
+c=3+10.5j
+print(a)
+print(b)
+print(c)
+print(a+b)'''
+#4
+'''a=True     #true is one and b is zero
+b=False
+print(a)
+print(b)
+print(a+a)
+print(a+b)'''
+#5
+"""s1='hari'
+s2="hari"
+s3='''hari
+is
+great'''
+print(s1)
+print(s2)
+print(s3)
+print(s4) """
+#6 creating a bytes datatype
+'''x=[10,20,30,100,200]
+y=bytes(x)
+print(type(y))
+print(y[0])
+print(y[1])'''
+#7 printing the byte data type values using for loop
+'''x=[12,56,69,18,45]
+y=bytes(x)
+for r in y :
+    print(r)'''
+# some error statement which represents that the values that are assnged cannt be changed
+'''x=[10,20,30,76]
+y=bytes(x)
+y[0]=30  '''# here im trying to change the value of 10 to 30 but now that it has been converted in bytes it cannot be changed
