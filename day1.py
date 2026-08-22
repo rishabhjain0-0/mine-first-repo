772d79766fb7eef3c541cc56714515d80e16daca
Rishabh Jain <rishabhjainqt@gmail.com>
2026-08-22 16:49:04 +0530

    day2

--- day1.py
+++ day1.py
@@ -53,3 +53,44 @@ for r in y :
 '''x=[10,20,30,76]
 y=bytes(x)
 y[0]=30  '''# here im trying to change the value of 10 to 30 but now that it has been converted in bytes it cannot be changed
+#7 creating a range of values using range 
+'''a=range(5)
+print(a)
+for x in a :
+    print(x)'''
+#8
+'''a=12
+print(int("23")+a)
+print("23"+str(a))
+
+print(bool(23)+4) #any numberis true except 0 or none 
+print(bool("")+4)''' #string is true except empty string
+#8
+'''a=5            #Type Conversion
+print(float(a))
+b=6.9
+print(int(b))
+print(str(a))'''
+#9   nesting
+'''a=10
+b=20
+c=-8
+d=-7
+e=((a if a<d else d) if a<c else (c if c<d else d))if a<b else ((b if b<d else d) if b<c else (c if c<d else d))
+print (e)'''
+# special operators
+'''a=6
+b=4
+print(a**b)  # a to the power of b
+print(a//b)  # divides and gives only the integer value
+print(a%b)'''   # divides and gives the remainder 
+# and function
+'''print(0 and 4)
+print(4 and 7)
+print(21 and 0)
+print(15 and 8)'''
+# some more operators (also works for-,*,%,/)
+'''a=12
+a+=5
+print(a)'''
+
